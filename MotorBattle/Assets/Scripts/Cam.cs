b6dde6d9d773c58db8cca0116f3cbe39c23b9c49
NeuralPrg <chabaneyann@gmail.com>
2022-09-18 22:19:23 +0200

    #8

--- MotorBattle/Assets/Scripts/Cam.cs
+++ MotorBattle/Assets/Scripts/Cam.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class Cam : MonoBehaviour
 {
     public AudioClip boomFX;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("camShaking", false);
+    }
 
     public void PlayBoomFX()
     {
         GetComponent<AudioSource>().PlayOneShot(boomFX);
+        anim.SetBool("camShaking", true);
+        Invoke("StopShaking", 1);
+    }
+
+    void StopShaking()
+    {
+        anim.SetBool("camShaking", false);
     }
 }
